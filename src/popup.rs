5f776fb58f407775a0fbb9d7bb41befb0a06c71e
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2024-11-04 19:01:58 +0000

    feat: iter to create buttons

--- src/popup.rs
+++ src/popup.rs
@@ -30,13 +30,13 @@ impl PopupButton {
 }
 
 pub struct Popup {
-    selected_button: usize,
+    selected_button_index: usize,
     buttons: Vec<PopupButton>,
 }
 impl Popup {
     pub fn new() -> Self {
         Self {
-            selected_button: 0,
+            selected_button_index: 0,
             buttons: vec![
                 PopupButton::Cancel,
                 PopupButton::ExitWithoutSaving,
@@ -46,18 +46,18 @@ impl Popup {
     }
 
     pub fn next_button(&mut self) {
-        self.selected_button = (self.selected_button + 1) % 3;
+        self.selected_button_index = (self.selected_button_index + 1) % 3;
     }
 
     pub fn previous_button(&mut self) {
-        if self.selected_button == 0 {
-            self.selected_button = 2;
+        if self.selected_button_index == 0 {
+            self.selected_button_index = 2;
         } else {
-            self.selected_button -= 1;
+            self.selected_button_index -= 1;
         }
     }
     pub fn select_button(&self) -> PopupButton {
-        match self.selected_button {
+        match self.selected_button_index {
             0 => PopupButton::Cancel,
             1 => PopupButton::ExitWithoutSaving,
             2 => PopupButton::ExitWithSave,
@@ -72,20 +72,20 @@ impl Popup {
         let popup_block = Block::default()
             .borders(Borders::ALL)
             .border_style(Style::default().fg(Color::Blue));
-    
+
         frame.render_widget(Clear, popup_area);
         frame.render_widget(popup_block, popup_area);
-    
+
         let inner_chunks = Layout::default()
             .direction(Direction::Vertical)
             .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
             .split(popup_area);
         let popup_text = Paragraph::new(
             "You are about to exit program. What would you like to do with current changes?",
-        ).block(Block::new().padding(Padding::new(0, 0, 0, 0)))
+        )
+        .block(Block::new().padding(Padding::new(0, 0, 0, 0)))
         .alignment(Alignment::Center);
         frame.render_widget(popup_text, inner_chunks[0]);
-    
         let button_chunks = Layout::default()
             .direction(Direction::Horizontal)
             .constraints(
@@ -97,18 +97,19 @@ impl Popup {
                 .as_ref(),
             )
             .split(inner_chunks[1]);
-        let button_text = Paragraph::new(PopupButton::Cancel.label()).block(
-            Block::new()
-                .style(Style::new().bg(Color::Black))
-                .padding(Padding::new(
-                    button_chunks[0].width / 2,
-                    0,
-                    button_chunks[0].height / 2,
-                    0,
-                ))
-                .style(PopupButton::Cancel.style()),
-        );
-        frame.render_widget(button_text, button_chunks[0]);
+
+        for (i, &button) in self.buttons.iter().enumerate() {
+            let button_style = if i == self.selected_button_index {
+                Style::default().fg(Color::Black).bg(Color::Yellow)
+            } else {
+                button.style()
+            };
+
+            let button_text = Paragraph::new(button.label())
+                .style(button_style)
+                .block(Block::default().borders(Borders::ALL));
+
+            frame.render_widget(button_text, button_chunks[i]);
+        }
     }
 }
-
